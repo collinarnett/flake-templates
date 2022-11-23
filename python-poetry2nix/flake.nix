5fdcab946c4bb6e0d84fcfe4a8ac85402ec6fecc
Collin Arnett <collin@arnett.it>
2022-11-23 00:18:39 -0500

    Added python-poetry2nix

---
+++ python-poetry2nix/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A ";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    poetry2nix,
+  }: let
+    system = "x86_64-linux";
+    overlay = nixpkgs.lib.composeManyExtensions [
+      poetry2nix.overlay
+      (final: prev: {
+        poetry-app = prev.poetry2nix.mkPoetryApplication {
+          projectDir = ./.;
+        };
+      })
+    ];
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [overlay];
+    };
+  in {
+    apps.${system}.default = {
+      type = "app";
+      program = "${pkgs.poetry-app}/bin/poetry_app";
+    };
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        (python310.withPackages (ps: with ps; [poetry black]))
+      ];
+    };
+  };
+}
